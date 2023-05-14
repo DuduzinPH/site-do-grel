461339faba098c03ae8b5cef312a7a349eeae475
Gabriel_Tomaz <tomazlemosgarcia@gmail.com>
2023-05-14 02:30:59 -0300

    continuado

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
 
-areaParticipantes = 
-document.getElementsByClassName("areaParticipantes").item(0);
+areaParticipantes =
+    document.getElementsByClassName("areaParticipantes").item(0);
 
+Alpha = {
+    nome: "ALPHA(1)",
+    participantes: ["Dabson", "Lucas", "Rillary"]
+}
 
+lotus = {
+    nome: "lotus",
+    participantes: ['Guerra', 'Tomaz', 'Dudu', 'Yan']
+}
 
-
-Alpha = {nome:"ALPHA(1)",
-participantes:["Dabson","Lucas","Rillary"]}
-
-lotus = {nome:"lotus",
-participantes:['Guerra','Tomaz','Dudu','Yan']}
-
-equipes = [Alpha,lotus]
+equipes = [{ nome: TechLadies, participantes: [Violetta, Rillary,Dayane,Erika] },
+{ nome:lotus, participantes: [Eduardo,Tomaz,Guerra,Yan] },
+{ nome:BKP, participantes: [Andrey,Airam,David] },
+{ nome:, participantes: [] }]
 
 var texto = '';
 
 equipes.forEach(element => {
     texto = texto + '<tr>'
     texto = texto + '<td>' + element.nome + '</td> <td>'
-    element.participantes.forEach(element=>{
+    element.participantes.forEach(element => {
         console.log(texto);
         texto = texto + element + '<br>';
     })
     texto = texto + '</td> </tr>'
 });
 
-areaParticipantes.innerHTML = "<tr><td>"+ texto+"</td></tr>"
+areaParticipantes.innerHTML = "<tr><td>" + texto + "</td></tr>"
 
-//areaParticipantes = "<tr><td> \n\n\naaaaaaaaaaaa </td></tr>"
